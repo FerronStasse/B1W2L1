7fe296fe1ee2383ebeaa3a69451248bca40185df
Ferron <99052055@mydavinci.nl>
2020-09-08 11:36:06 +0200

    b1w2l2 toegevoegd

---
+++ B1W2L2/b1w2l2.js
@@ -0,0 +1,10 @@
+// begin hier je JavaScript commandos
+// Ferron Stasse
+// opleading AO
+
+	        const askfirstname = prompt("What is your firstname")
+	    	const asklastname =  prompt("what is your lastname")
+	    	const askage =       prompt("what is your age")
+
+	    	var answer = "Firstname: " + askfirstname + "<br>" + "Lastname: " + asklastname + "<br>" + "Age: " + askage
+	    	document.write(answer)
