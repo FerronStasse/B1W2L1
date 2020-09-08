9a14d12431f7973617331c9bcf7c7b9f8f68e9f8
Ferron <99052055@mydavinci.nl>
2020-09-08 11:33:15 +0200

    b1w2l1 toegevoed

---
+++ B1W2L1/b1w2l1.js
@@ -0,0 +1,8 @@
+// begin hier je JavaScript commandos
+// Ferron Stasse
+// opleading AO
+
+	    	var firstname = prompt("What is your firstname"); //pop-up met een vraag naar je voornaam.
+	    	var lastname = prompt("What is your lastname"); //pop-up met een vraag naar je achternaam.
+	    	var age = prompt("What is your age"); //pop-up met een vraag naar je leefttijd.
+	    	document.write("Firstname: " + firstname + "<br>" + "Lastname: " + lastname + "<br>" + "Age: " + age) //na alle pop-up wordt het over geschrijven naar html en laat zien elke antwoorden je heb getyped en onder elkaar gezet met de vragen erbij.
